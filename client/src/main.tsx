90cf2a56322d5f9d95aea8e0dcef3f27559c61e5
Ruslan Volkov <volkov232002@gmail.com>
2026-07-10 15:09:52 +0000

    debug(nav): временный оверлей логирует все навигации, чтобы поймать причину баг с /payment-methods
    Активируется через ?debug=nav в URL. Логирует pushState, replaceState,
    popstate, location.assign, location.replace, все клики по <a> и <button>,
    overlay:back event. Показывает fixed overlay в левом нижнем углу с
    последними 15 событиями. Кнопка off отключает через localStorage.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { installNavDebug } from "./nav-debug";
+
+installNavDebug();
 
 createRoot(document.getElementById("root")!).render(<App />);
