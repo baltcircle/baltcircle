d53bf70cb20b11a809154c903bf97004a581d753
Ruslan Volkov <volkov232002@gmail.com>
2026-07-10 15:10:15 +0000

    chore: убрать временный nav-debug — причина найдена (баннер оплаты)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { installNavDebug } from "./nav-debug";
-
-installNavDebug();
 
 createRoot(document.getElementById("root")!).render(<App />);
